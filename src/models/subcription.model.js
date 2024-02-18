ebd10091d125286439037f3c577b6d0013c547ba
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-02-19 02:05:54 +0530

    backend
    backend

---
+++ src/models/subcription.model.js
@@ -0,0 +1,15 @@
+import mongoose,{Schema} from "mongoose";
+
+const subcriptionSchema =new Schema({
+    subscriber : {
+        type :Schema.Types.ObjectId, //one who is subscribing
+        ref : "User"
+    },
+    channel:{
+        type: Schema.Types.ObjectId, //one to whom subscriber is subscribing
+        ref: "User"
+    }
+} , {timestamps : true})
+
+
+export const Subcription = mongoose.model("Subcription" , subcriptionSchema)
